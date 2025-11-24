b6a4c2929f6b2c6bcf97ebf4c14b8df76195b048
Harsh Amin <aminharsh317@gmail.com>
2025-11-24 13:45:41 -0800

    update dependencies and imports for new versions

--- cdk/lambda/textGeneration/src/helpers/chat.py
+++ cdk/lambda/textGeneration/src/helpers/chat.py
@@ -5,7 +5,8 @@ import time
 from langchain_aws import ChatBedrock, BedrockLLM
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains import create_retrieval_chain, create_history_aware_retriever
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_community.chat_message_histories import DynamoDBChatMessageHistory
 from langchain_core.pydantic_v1 import BaseModel, Field
